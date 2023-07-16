4054a232b51cf1f17461821837f1afa712b937ac
codelab21 <codelab21@outlook.com>
2023-07-16 13:43:15 +0300

    Initial Commit

---
+++ WeatherAppTutorial/WeatherAppTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppTutorialApp.swift
+//  WeatherAppTutorial
+//
+//  Created by Eymen on 16.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
